66f593dd59e7f079ceb8fb7eac07d3b2c691bee1
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-16 17:07:38 +0600

    week 03 day 01 problem 1 & 2 added

---
+++ week 03/Day 01/P1-Longest Subarray With Sum K .cpp
@@ -0,0 +1,25 @@
+// problem link : https://www.codingninjas.com/codestudio/problems/longest-subarray-with-sum-k_6682399?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems
+
+int longestSubarrayWithSumK(vector<int> a, long long k){
+    int n=a.size();
+    int i=0,j=0;
+    long long sum = 0;
+    int ans=0;
+
+    while(j<n)
+    {
+        sum +=a[j];
+        if(sum>k)
+        {
+            while(sum>k)
+            {
+                sum-=a[i];
+                i++;
+            }
+        }
+        if(sum == k)
+            ans=max(ans,j-i+1);
+        j++;
+    }
+    return ans;
+}
