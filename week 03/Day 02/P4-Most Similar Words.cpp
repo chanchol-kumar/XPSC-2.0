df1707bf8e5af4eb5a7e297007b58f681a9290f6
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-17 14:48:39 +0600

    Week 03 Day 02 Problem 04 added..

--- week 03/Day 02/P4-Most Similar Words.cpp
+++ week 03/Day 02/P4-Most Similar Words.cpp
@@ -1,1 +1,41 @@
 //problem link : https://codeforces.com/contest/1676/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        string a[n];
+
+        int ans=INT_MAX,dis,sum;
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                sum = 0;
+                for(int k=0;k<x;k++)
+                {
+                    dis=abs(a[i][k]-a[j][k]);
+                    sum+=dis;
+                }
+                if(sum < ans)
+                    ans = sum;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
