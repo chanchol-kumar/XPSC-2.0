c941a6dd313bcb2f7c9c20702d6321bd1deb45bb
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-17 14:10:37 +0600

    week 03 day 02 problem 03 added..

--- week 03/Day 02/P3-All Distinct.cpp
+++ week 03/Day 02/P3-All Distinct.cpp
@@ -1,1 +1,35 @@
 //problem link : https://codeforces.com/contest/1692/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        set<int>s;
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+            s.insert(arr[i]);
+        }
+
+        if((n-s.size())%2==0)
+        {
+            cout<<s.size()<<endl;
+        }
+        else
+            cout<<s.size()-1<<endl;
+    }
+    return 0;
+}
