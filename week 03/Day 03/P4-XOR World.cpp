d7a7f1f430277c5ba2bc831b25483672961f5888
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-18 15:57:14 +0600

    Week 03 Day 03 Problem 04 added...

--- week 03/Day 03/P4-XOR World.cpp
+++ week 03/Day 03/P4-XOR World.cpp
@@ -1,1 +1,28 @@
 //problem link : https://atcoder.jp/contests/abc121/tasks/abc121_d?lang=en
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+long long XOR(long long n)
+{
+    if(n%4==0) return n;
+    else if(n%4==1) return 1;
+    else if(n%4==2) return n+1;
+    else return 0;
+}
+
+int main()
+{
+    optimize()
+    long long a,b;
+    cin>>a>>b;
+    if(a==0){
+        cout<<XOR(b)<<endl;
+    }else
+        cout<<(XOR(b) ^ XOR(a-1))<<endl;
+
+    return 0;
+}
