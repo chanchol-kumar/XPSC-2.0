a5dd26402e2766ff2f5614aedecc03ddca6e5813
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-19 23:29:35 +0600

    Week 03 Day 04 Problem 05 added...

---
+++ week 03/Day 04/P5 - Different Differences.cpp
@@ -0,0 +1,33 @@
+//problem link : https://codeforces.com/contest/1772/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int k,n;
+        cin>>k>>n;
+        int j=1,d=1;
+        for(int i=1; i<=k; i++)
+        {
+            cout<<j<<" ";
+            if(n-(j+d)>=(k-i-1))
+            {
+                j+=d;
+                d++;
+            }
+            else
+                j++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
