bfbc48c5ebe2ba74bcd293e76e2dc0789a9dfb4f
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-25 15:37:21 +0600

    Week 04 Day 03 Problem 02 added...

---
+++ week 04/Day 03/P2 - Minimum Varied Number.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string str;
+        for(int j=9;j>=1;j--)
+        {
+            if(n>=j)
+            {
+                str +=(char)('0'+j);
+                n-=j;
+            }
+        }
+        reverse(str.begin(),str.end());
+        cout<<str<<endl;
+    }
+    return 0;
+}
