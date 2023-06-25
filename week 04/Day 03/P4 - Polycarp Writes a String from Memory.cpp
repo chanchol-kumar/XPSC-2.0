5c443360e1c718b450ba56463e4d3cfa4d6771bf
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-25 15:39:57 +0600

    Week 04 Day 03 Problem 04 added...

---
+++ week 04/Day 03/P4 - Polycarp Writes a String from Memory.cpp
@@ -0,0 +1,38 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    typedef long long int ll;
+     
+    #define endl '\n';
+    #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+     
+    int main()
+    {
+        optimize();
+        int t;
+        cin>>t;
+        while(t--)
+        {
+            string s;
+            cin>>s;
+            set<char>st;
+            int cnt=0;
+            for(int i=0; i<s.size(); i++)
+            {
+                st.insert(s[i]);
+                if(st.size()==4)
+                {
+                    cnt++;
+                    i--;
+                    st.clear();
+                }
+            }
+     
+            if(st.empty())
+            {
+                cout<<cnt<<endl;
+            }
+            else
+                cout<<cnt+1<<endl
+            }
+        return 0;
+    }
