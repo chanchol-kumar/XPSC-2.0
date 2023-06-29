03bde5f2ede86cc30ef37a07b42523315c6a2a5c
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-29 18:13:07 +0600

    Week 01 Day 02 Problem 02 added...

---
+++ week 01/Day 02/P2 - Counting.cpp
@@ -0,0 +1,17 @@
+//problem link : https://atcoder.jp/contests/abc209/tasks/abc209_a?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    if(a<=b)
+        cout<<b-a+1<<endl;
+    else
+        cout<<0<<endl;
+
+    return 0;
+}
