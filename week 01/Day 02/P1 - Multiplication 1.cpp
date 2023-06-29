f7dbe0bd2a8feee0765ca570c592354fcfebf735
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-29 18:08:08 +0600

    Week 01 Day 02 Problem 01 added...

---
+++ week 01/Day 02/P1 - Multiplication 1.cpp
@@ -0,0 +1,13 @@
+//problem link : https://atcoder.jp/contests/abc209/tasks/abc209_a?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+
+    return 0;
+}
