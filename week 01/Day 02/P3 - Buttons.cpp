20a1bb61a4a8e4b4a28f41bec68d5a23df6de1a0
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-30 11:41:03 +0600

    Week 01 Day 02 Problem 03 added...

---
+++ week 01/Day 02/P3 - Buttons.cpp
@@ -0,0 +1,14 @@
+//problem link : https://atcoder.jp/contests/abc124/tasks/abc124_a?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+ 
+    cout<<max({a+a-1,b+b-1,a+b})<<endl;
+ 
+    return 0;
+}
