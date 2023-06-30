9b1cc039fa42d21d358613e041077910d7a7b149
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-06-30 11:43:27 +0600

    Week 01 Day 02 Problem 04 added...

---
+++ week 01/Day 02/P4 - Round decimals.cpp
@@ -0,0 +1,14 @@
+//problem link : https://atcoder.jp/contests/abc226/tasks/abc226_a?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    double n;
+    cin>>n;
+ 
+    cout<<round(n)<<endl;
+ 
+    return 0;
+}
