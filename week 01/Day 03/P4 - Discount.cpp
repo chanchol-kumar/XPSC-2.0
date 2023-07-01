b4ccabc8f1f8e54c8badede6393894fe7cb09d9e
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-01 23:16:42 +0600

    Week 01 Day 03 Problem 04 added...

---
+++ week 01/Day 03/P4 - Discount.cpp
@@ -0,0 +1,13 @@
+//problem link : https://atcoder.jp/contests/abc193/tasks/abc193_a?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a,b;
+    cin>>a>>b;
+    cout<<100-(b*100)/a<<endl;
+
+    return 0;
+}
