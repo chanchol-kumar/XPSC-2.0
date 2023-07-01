a9922a05172306185a61dd663e43ba915c3fc6a4
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-01 23:23:17 +0600

    Week 01 Day 04 Problem 01 added...

---
+++ week 01/Day 04/P4 - Biscuit Generator.cpp
@@ -0,0 +1,13 @@
+//problem link : https://atcoder.jp/contests/abc125/tasks/abc125_a?lang=en
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,n;
+    cin>>a>>b>>n;
+    cout<<n/a*b<<endl;
+
+    return 0;
+}
