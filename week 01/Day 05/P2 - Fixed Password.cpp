506194b262e1944eb6a2a066f7b083d837ce6184
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-01 23:42:05 +0600

    Week 01 Day 05 Problem 02 added...

---
+++ week 01/Day 05/P2 - Fixed Password.cpp
@@ -0,0 +1,21 @@
+//problem link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==1999)
+        {
+            cout<<"Correct\n";
+            return 0;
+        }
+        else
+            cout<<"Wrong\n";
+    }
+
+    return 0;
+}
