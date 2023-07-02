b50a08605acc9d44f6b0bdd71e71f4093d7c0e2a
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-02 20:09:00 +0600

    Week 01 Day 07 Problem 07 added...

---
+++ week 01/Day 07/P7 - Sort String.cpp
@@ -0,0 +1,32 @@
+//problem link : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/O
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int n;
+    cin>>n;
+    int arr[123]={};
+    for(int i=0; i<n; i++)
+    {
+        char x;
+        cin>>x;
+        arr[x]++;
+    }
+    for(int i=0; i<123; i++)
+    {
+        if(arr[i]!=0)
+        {
+            while(arr[i]--)
+                cout<<(char)(i);
+        }
+    }
+    cout<<"\n";
+    return 0;
+}
