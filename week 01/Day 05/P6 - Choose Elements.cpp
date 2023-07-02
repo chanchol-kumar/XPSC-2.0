20e3c33cc2fa1764395302f8e6326fe4e86f2ace
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-02 20:40:06 +0600

    Week 01 Day 5 Problem 06 added....

---
+++ week 01/Day 05/P6 - Choose Elements.cpp
@@ -0,0 +1,29 @@
+//problem link : https://codeforces.com/group/MWSDmqGsZm/contest/329103/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    ll n,x,sum=0;
+    cin>>n>>x;
+
+    ll arr[n];
+    for(ll i=0; i<n; i++)
+        cin>>arr[i];
+
+    sort(arr,arr+n);
+    reverse(arr, arr+n);
+    for(ll i=0; i<x; i++)
+    {
+        if(arr[i]<0)break;
+        sum+=arr[i];
+    }
+    cout<<sum<<endl;
+    return 0;
+}
