5642bab89a67dc742fe512084e569c1f82c043e6
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-02 12:41:01 +0600

    Week 01 Day 03 Problem 03 added...

---
+++ week 01/Day 06/P3 - Smallest Pair.cpp
@@ -0,0 +1,38 @@
+//problem link : https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/I
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+ 
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+ 
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+ 
+        int min_val = INT_MAX;
+        for(int i=0; i<n-1; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int res = arr[i]+arr[j]+j-i;
+                if(res<min_val)
+                    min_val=res;
+            }
+        }
+        cout<<min_val<<endl;
+    }
+ 
+    return 0;
+}
