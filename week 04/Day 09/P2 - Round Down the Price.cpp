828656dfc8430a33694a5643d179294fd1f85497
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-03 14:21:36 +0600

    Week 04 Day 09 Problem 02 added...

---
+++ week 04/Day 09/P2 - Round Down the Price.cpp
@@ -0,0 +1,27 @@
+//problem link : https://codeforces.com/contest/1702/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,sum=0,i=0;
+        cin>>n;
+
+        while(n>=pow(10,i))
+        {
+            sum =n-pow(10,i);
+            i++;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
