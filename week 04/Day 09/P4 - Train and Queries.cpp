3aa66297f32dcdd3c86b1ddfc1e29664d44fed70
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-03 14:55:27 +0600

    Week 04 Day 09 Problem 04 added...

---
+++ week 04/Day 09/P4 - Train and Queries.cpp
@@ -0,0 +1,38 @@
+//problem link : https://codeforces.com/contest/1702/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,q;
+        cin>>n>>q;
+        map<int,int>left,right;
+        for(int i=1; i<=n; i++)
+        {
+            int x;
+            cin >> x;
+            if(left[x]==0)
+                left[x]=i;
+            right[x]=i;
+        }
+        while(q--)
+        {
+            int a,b;
+            cin>>a>>b;
+            if(left[a] && right[b] && left[a]<right[b])
+                cout<<"YES"<<endl;
+            else
+                cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
