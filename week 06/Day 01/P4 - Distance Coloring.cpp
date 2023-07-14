7b5981c7482f2693fe66f8504fca4de3c09ac746
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-14 22:19:26 +0600

    Week 06 Day 01 Problem 05 added...

---
+++ week 06/Day 01/P4 - Distance Coloring.cpp
@@ -0,0 +1,33 @@
+//problem link : https://www.codechef.com/problems/DISTANCECOLO
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        const unsigned int MOD = 1e9+7;
+        ll n,k,cnt=1;
+        cin>>n>>k;
+
+        for(int i=0; i<n; i++)
+        {
+            if(k>0)
+            {
+                cnt=(k*cnt)%MOD;
+                k--;
+            }
+            else
+                break;
+        }
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
