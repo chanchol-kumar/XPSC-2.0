e090a04d69fd9840ac2f46c6a93306d9663258a9
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-16 21:14:41 +0600

    Week 06 day 03 Problem 01 added...

---
+++ week 06/Day 03/P1 - Learning SQL .cpp
@@ -0,0 +1,18 @@
+//problem link : https://www.codechef.com/problems/LEARNSQL
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout<<(a+c)*b<<endl;
+
+    return 0;
+}
