73143a033d3c73a3bc624447149436647b460554
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-20 19:58:37 +0600

    Week 06 Day 07 Problem 03 added...

---
+++ week 06/Day 07/P3 - Buy Ten Packets.cpp
@@ -0,0 +1,21 @@
+//problem link : https://www.codechef.com/problems/TENPACKETS
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<2*b+a<<endl;
+    }
+    return 0;
+}
