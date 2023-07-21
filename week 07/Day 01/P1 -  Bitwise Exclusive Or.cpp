b0f18702d6936a0df7d50f7be30e7c9c21157782
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-21 14:09:23 +0600

    Week 07 Day 01 Problem 01 added...

---
+++ week 07/Day 01/P1 -  Bitwise Exclusive Or.cpp
@@ -0,0 +1,17 @@
+//problem 01 : https://atcoder.jp/contests/abc213/tasks/abc213_a?lang=en
+
+  #include<bits/stdc++.h>
+  using namespace std;
+  typedef long long int ll;
+   
+  #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+   
+  int main()
+  {
+      optimize();
+      int a,b;
+      cin>>a>>b;
+      cout<<(a^b)<<endl;
+    
+      return 0;
+  }
