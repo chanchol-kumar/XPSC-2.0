775576377917e20feb7a973f6418c69eff8b517b
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-22 21:31:31 +0600

    Week 07 Day 02 Problem 04 added...

---
+++ week 07/Day 02/P4 - Maximise Score.cpp
@@ -0,0 +1,36 @@
+//problem link : https://www.codechef.com/problems/MAXIMSCORE?tab=statement
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x,mx= INT_MAX;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int i=0;i<n;i++)
+        {
+            if(i==0)
+                x = max(abs(arr[i]-arr[i+1]),0);
+            else if(i==n-1)
+                x = max(abs(arr[i]-arr[i-1]),0);
+            else
+                x = max(abs(arr[i]-arr[i-1]), abs(arr[i]-arr[i+1]));
+
+            mx = min(mx,x);
+        }
+        cout<<mx<<endl;
+    }
+
+    return 0;
+}
