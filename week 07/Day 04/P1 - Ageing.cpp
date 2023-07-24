923b6c851428831a5623b95652654bdd8bc33bcf
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-07-24 22:38:04 +0600

    Week 07 Day 04 Problem 01 added...

---
+++ week 07/Day 04/P1 - Ageing.cpp
@@ -0,0 +1,21 @@
+//problem link : https://www.codechef.com/problems/AGEING
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n-10<<endl;
+    }
+    return 0;
+}
