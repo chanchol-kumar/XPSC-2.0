df9e8e55ad4958f6adecc2248602b1c2cd966065
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-08-12 13:56:24 +0600

    Week 10 Day 02 Problem 02 added...

---
+++ week 10/Day 02/P1 - CQXYM Count Permutations.cpp
@@ -0,0 +1,29 @@
+//problem link : https://codeforces.com/contest/1581/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define MOD 1000000007
+
+int main()
+{
+    optimize();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        ll fact = 1;
+        for (int i = 3; i <= 2 * n; i++)
+        {
+            fact *= i;
+            fact %= MOD;
+        }
+        cout << fact << endl;
+    }
+    return 0;
+}
