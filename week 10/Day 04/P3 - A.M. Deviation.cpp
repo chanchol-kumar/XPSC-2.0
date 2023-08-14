aeae23cd3537b81389d3f8720b952835d73adf79
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-08-14 23:15:41 +0600

    Week 10 Day 04 Problem 03 added...

---
+++ week 10/Day 04/P3 - A.M. Deviation.cpp
@@ -0,0 +1,24 @@
+//problem link : https://codeforces.com/contest/1605/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if ((a + b - c * 2) % 3 == 0)
+            cout << "0\n";
+        else
+            cout << "1\n";
+    }
+    return 0;
+}
