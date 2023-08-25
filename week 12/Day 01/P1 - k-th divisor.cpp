c823a4767a1619f248aaeb50965d7dcc593655cb
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-08-25 23:39:09 +0600

    Week 12 Day 01 Problem 01 added...

--- week 12/Day 01/P1 - k-th divisor.cpp
+++ week 12/Day 01/P1 - k-th divisor.cpp
@@ -1,2 +1,31 @@
 //problem link : https://codeforces.com/problemset/problem/762/A
 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> arr;
+    for (ll i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            arr.push_back(i);
+            if ((n / i) != i)
+                arr.push_back(n / i);
+        }
+    }
+    sort(arr.begin(), arr.end());
+    if (arr.size() < k)
+        cout << -1 << '\n';
+    else
+        cout << arr[k - 1] << '\n';
+    return 0;
+}
