435bf3e671708d0b0200f1b490a36049ccf83bf1
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-08-27 16:33:46 +0600

    Week 12 Day 03 Problem 04 added...

---
+++ week 12/Day 03/P4 - Bachgold Problem.cpp
@@ -0,0 +1,29 @@
+// Problem link : https://codeforces.com/problemset/problem/749/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, ans = 0;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n / 2 << endl;
+        for (int i = 0; i < n / 2; i++)
+            cout << 2 << " ";
+        cout << "\n";
+    }
+    else
+    {
+        cout << n / 2 << endl;
+        for (int i = 0; i < n / 2 - 1; i++)
+            cout << 2 << " ";
+        cout << 3 << "\n";
+    }
+    return 0;
+}
