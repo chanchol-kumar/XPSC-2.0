dc9933fb7829505d5e71bc9519f9c4a95c94009c
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-09-11 18:55:31 +0600

    Create P2 - Minimum LCM.cpp

---
+++ week 14/Day 04/P2 - Minimum LCM.cpp
@@ -0,0 +1,29 @@
+// problem link : https://codeforces.com/problemset/problem/1765/M
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a = 1;
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                a = n / i;
+                break;
+            }
+        }
+        cout << a << ' ' << n - a << '\n';
+    }
+    return 0;
+}
