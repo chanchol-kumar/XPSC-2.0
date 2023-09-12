5ded364de1323c229298e1fc97a0678e39d85e0c
Chanchol Kumar Modok <65625498+chanchol-kumar@users.noreply.github.com>
2023-09-12 22:25:31 +0600

    Week 14 Day 05 Problem 01 added...

---
+++ week 14/Day 05/P1 - green_gold_dog, array and permutation .cpp
@@ -0,0 +1,38 @@
+//problem link : https://codeforces.com/contest/1867/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            arr.push_back({x, i});
+        }
+      
+        sort(arr.begin(), arr.end());
+        for (int i = 0, j = n; i < n; i++, j--)
+            arr[i].first = j;
+
+        int ans[n];
+        for (int i = 0; i < n; i++)
+            ans[arr[i].second] = arr[i].first;
+
+        for (int i = 0; i < n; i++)
+            cout << ans[i] << ' ';
+        cout << endl;
+    }
+    return 0;
+}
